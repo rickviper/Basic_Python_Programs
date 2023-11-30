fe542641993887c86ca6d960c573b329814c6b26
rickviper <66552399+rickviper@users.noreply.github.com>
2023-11-30 19:58:21 +0530

    Create Positive or negative num

---
+++ Positive or negative num.py
@@ -0,0 +1,6 @@
+#To find whether a number is positive or negative
+num = int(input("Enter a number: "))
+if num > 0:
+    print(num,"is a positive number.")
+else:
+    print(num,"is a negative number.")
