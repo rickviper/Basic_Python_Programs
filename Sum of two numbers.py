9d448401ab570b23f7ded8d34c6637ef14ba7d64
rickviper <66552399+rickviper@users.noreply.github.com>
2023-11-30 20:01:23 +0530

    Sum of two numbers.py

---
+++ Sum of two numbers.py
@@ -0,0 +1,4 @@
+#Sum of 2 num 
+first_num = int(input("Enter first number: "))
+second_num = int(input("Enter second number: "))
+print("The sum of", first_num, "and", second_num, "is", first_num + second_num)
