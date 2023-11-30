836c407a54a1560d82533be3edb9b6aa715237a0
rickviper <66552399+rickviper@users.noreply.github.com>
2023-11-30 20:04:13 +0530

    Sum of 2 numbers.py

---
+++ Sum of 2 numbers.py
@@ -0,0 +1,6 @@
+#Sum of 2 numbers using functions
+def sum(a,b):
+    print("The sum of",num1, "and",num2,"is", a + b)
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter the second number: "))
+sum(num1,num2)
